a95cd5bd0247cb1bd12a530b5d67539f110624c9
Nick Consolo <nick.consolo@gmail.com>
2012-01-26 01:12:40 -0500

    fixing formatting issues from copy/paste

--- nessus-extractor.php
+++ nessus-extractor.php
@@ -11,25 +11,18 @@ $xmlData = new SimpleXMLElement($xmlFileData);
 // print_r($xmlData);
 
 // Show Report Settings
-echo 
-",-----------------------------------------------------------------------------------------,\r\n";
-echo "| Nessus Server Settings                                                                  
-|\r\n";
-echo 
-"'-----------------------------------------------------------------------------------------'\r\n\r\n";
-foreach($xmlData->Policy->Preferences->ServerPreferences->preference as 
-$preference) {
+echo ",-----------------------------------------------------------------------------------------,\r\n";
+echo "| Nessus Server Settings                                                                  |\r\n";
+echo "'-----------------------------------------------------------------------------------------'\r\n\r\n";
+foreach($xmlData->Policy->Preferences->ServerPreferences->preference as $preference) {
         echo $preference->name . " = " . $preference->value . "\r\n";
 }
 
 // Show Plugin Preferences
 echo "\r\n";
-echo 
-",-----------------------------------------------------------------------------------------,\r\n";
-echo "| Nessus Server Plugin Preferences                                                        
-|\r\n";
-echo 
-"'-----------------------------------------------------------------------------------------'\r\n\r\n";
+echo ",-----------------------------------------------------------------------------------------,\r\n";
+echo "| Nessus Server Plugin Preferences                                                        |\r\n";
+echo "'-----------------------------------------------------------------------------------------'\r\n\r\n";
 foreach($xmlData->Policy->Preferences->PluginsPreferences->item as 
 $item) {
         echo "Plug-in ID: " . $item->pluginId . "\r\n";
@@ -37,19 +30,15 @@ $item) {
         echo "Full Plug-In Name: " . $item->fullName . "\r\n";
         echo "Preference Name: " . $item->preferenceName . "\r\n";
         echo "Selected Value: " . $item->selectedValue . "\r\n";
-        echo 
-"-------------------------------------------------------------------------------------------\r\n";
+        echo "-------------------------------------------------------------------------------------------\r\n";
 }
 
 
 // Show Hosts Summary
 echo "\r\n";
-echo 
-",-----------------------------------------------------------------------------------------,\r\n";
-echo "| Nessus Scan - Hosts Summary                                                             
-|\r\n";
-echo 
-"'-----------------------------------------------------------------------------------------'\r\n\r\n";
+echo ",-----------------------------------------------------------------------------------------,\r\n";
+echo "| Nessus Scan - Hosts Summary                                                             |\r\n";
+echo "'-----------------------------------------------------------------------------------------'\r\n\r\n";
 
   foreach($xmlData->Report->ReportHost as $host) {
         $ports = array();
@@ -60,15 +49,13 @@ echo
         /* get unique ips */
         $dnsnext = 0;
         foreach ($host->HostProperties->tag as $thetag) {
-                if 
-(preg_match('/^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/',$thetag)) 
-{
+                if (preg_match('/^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/',$thetag)) 
+				{
                         $theip = $thetag;
                         $dnsnext = 1;
                 } else {
 
-                        if ($dnsnext == 1) { $thedns = $thetag; $dnsnext 
-= 0; }
+                        if ($dnsnext == 1) { $thedns = $thetag; $dnsnext = 0; }
                 }
         }
         echo $theip . " - " . $thedns . "\r\nOpen Ports: ";
@@ -98,98 +85,40 @@ $medvulnerabilities = array();
 $lowvulnerabilities = array();
 $infovulnerabilities = array();
 echo "\r\n";
-echo 
-",-----------------------------------------------------------------------------------------,\r\n";
-echo "| Nessus Scan - Vulnerabilities by Host                                                   
-|\r\n";
-echo 
-"'-----------------------------------------------------------------------------------------'\r\n\r\n";
+echo ",-----------------------------------------------------------------------------------------,\r\n";
+echo "| Nessus Scan - Vulnerabilities by Host                                                   |\r\n";
+echo "'-----------------------------------------------------------------------------------------'\r\n\r\n";
 
   foreach($xmlData->Report->ReportHost as $host) {
                 foreach ($host->HostProperties->tag as $thetag) {
-                if 
-(preg_match('/^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/',$thetag)) 
-{
+                if (preg_match('/^(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]).){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/',$thetag)) 
+				{
                         $theip = $thetag;
                         $dnsnext = 1;
                 } else {
 
-                        if ($dnsnext == 1) { $thedns = $thetag; $dnsnext 
-= 0; }
+                        if ($dnsnext == 1) { $thedns = $thetag; $dnsnext = 0; }
                 }
                 }
                 echo $theip . " - " . $thedns . "\r\n";
-                echo 
-"---------------------------------------------------------------\r\n";
+                echo "---------------------------------------------------------------\r\n";
                 foreach ($host->ReportItem as $item) {
                         if ($item->attributes()->severity == '3')
-                                $tmpvuln = array("ip" => $theip, "dns" 
-=> $thedns, "port" => $item->attributes()->port, "svc_name" => 
-$item->attributes()->svc_name, "protocol" => 
-$item->attributes()->protocol, "pluginID" => 
-$item->attributes()->pluginID, "pluginName" => 
-$item->attributes()->pluginName, "pluginFamily" => 
-$item->attributes()->pluginFamily, "vuln_publication_date" => 
-$item->vuln_publication_date, "cpe" => $item->cpe, "solution" => 
-$item->solution, "risk_factor" => $item->risk_factor, "description" => 
-$item->description, "plugin_publication_date" => 
-$item->plugin_publication_date, "cvss_vector" => $item->cvss_vector, 
-"synopsis" => $item->synopsis, "plugin_type" => $item->plugin_type, 
-"patch_publication_date" => $item->patch_publication_date, 
-"plugin_modification_date" => $item->plugin_modification_date, 
-"cvss_base_score" => $item->cvss_base_score, "cve" => $item->cve, "bid" 
-=> $item->bid, "xref" => $item->xref, "plugin_output" => 
-$item->plugin_output, "plugin_version" => $item->plugin_version);
+                                $tmpvuln = array("ip" => $theip, "dns" => $thedns, "port" => $item->attributes()->port, "svc_name" => $item->attributes()->svc_name, "protocol" => $item->attributes()->protocol, "pluginID" => $item->attributes()->pluginID, "pluginName" => $item->attributes()->pluginName, "pluginFamily" => $item->attributes()->pluginFamily, "vuln_publication_date" => $item->vuln_publication_date, "cpe" => $item->cpe, "solution" => $item->solution, "risk_factor" => $item->risk_factor, "description" => $item->description, "plugin_publication_date" => $item->plugin_publication_date, "cvss_vector" => $item->cvss_vector, "synopsis" => $item->synopsis, "plugin_type" => $item->plugin_type, "patch_publication_date" => $item->patch_publication_date, "plugin_modification_date" => $item->plugin_modification_date, "cvss_base_score" => $item->cvss_base_score, "cve" => $item->cve, "bid" => $item->bid, "xref" => $item->xref, "plugin_output" => $item->plugin_output, "plugin_version" => $item->plugin_version);
                                 $critvulnerabilities[] = $tmpvuln;
                         }
                         if ( $item->attributes()->severity == '2') {
-                                $tmpvuln = array("ip" => $theip, "dns" 
-=> $thedns, "port" => $item->attributes()->port, "svc_name" => 
-$item->attributes()->svc_name, "protocol" => 
-$item->attributes()->protocol, "pluginID" => 
-$item->attributes()->pluginID, "pluginName" => 
-$item->attributes()->pluginName, "pluginFamily" => 
-$item->attributes()->pluginFamily, "vuln_publication_date" => 
-$item->vuln_publication_date, "cpe" => $item->cpe, "solution" => 
-$item->solution, "risk_factor" => $item->risk_factor, "description" => 
-$item->description, "plugin_publication_date" => 
-$item->plugin_publication_date, "cvss_vector" => $item->cvss_vector, 
-"synopsis" => $item->synopsis, "plugin_type" => $item->plugin_type, 
-"patch_publication_date" => $item->patch_publication_date, 
-"plugin_modification_date" => $item->plugin_modification_date, 
-"cvss_base_score" => $item->cvss_base_score, "cve" => $item->cve, "bid" 
-=> $item->bid, "xref" => $item->xref, "plugin_output" => 
-$item->plugin_output, "plugin_version" => $item->plugin_version);
+                                $tmpvuln = array("ip" => $theip, "dns" => $thedns, "port" => $item->attributes()->port, "svc_name" => $item->attributes()->svc_name, "protocol" => $item->attributes()->protocol, "pluginID" => $item->attributes()->pluginID, "pluginName" => $item->attributes()->pluginName, "pluginFamily" => $item->attributes()->pluginFamily, "vuln_publication_date" => $item->vuln_publication_date, "cpe" => $item->cpe, "solution" => 
+$item->solution, "risk_factor" => $item->risk_factor, "description" => $item->description, "plugin_publication_date" => $item->plugin_publication_date, "cvss_vector" => $item->cvss_vector, "synopsis" => $item->synopsis, "plugin_type" => $item->plugin_type, "patch_publication_date" => $item->patch_publication_date, "plugin_modification_date" => $item->plugin_modification_date, "cvss_base_score" => $item->cvss_base_score, "cve" => $item->cve, "bid" => $item->bid, "xref" => $item->xref, "plugin_output" => $item->plugin_output, "plugin_version" => $item->plugin_version);
                                 $medvulnerabilities[] = $tmpvuln;
                         }
                         if ( $item->attributes()->severity == '1') {
-                                $tmpvuln = array("ip" => $theip, "dns" 
-=> $thedns, "port" => $item->attributes()->port, "svc_name" => 
-$item->attributes()->svc_name, "protocol" => 
-$item->attributes()->protocol, "pluginID" => 
-$item->attributes()->pluginID, "pluginName" => 
-$item->attributes()->pluginName, "pluginFamily" => 
-$item->attributes()->pluginFamily, "vuln_publication_date" => 
-$item->vuln_publication_date, "cpe" => $item->cpe, "solution" => 
-$item->solution, "risk_factor" => $item->risk_factor, "description" => 
-$item->description, "plugin_publication_date" => 
-$item->plugin_publication_date, "cvss_vector" => $item->cvss_vector, 
-"synopsis" => $item->synopsis, "plugin_type" => $item->plugin_type, 
-"patch_publication_date" => $item->patch_publication_date, 
-"plugin_modification_date" => $item->plugin_modification_date, 
-"cvss_base_score" => $item->cvss_base_score, "cve" => $item->cve, "bid" 
-=> $item->bid, "xref" => $item->xref, "plugin_output" => 
-$item->plugin_output, "plugin_version" => $item->plugin_version);
+                                $tmpvuln = array("ip" => $theip, "dns" => $thedns, "port" => $item->attributes()->port, "svc_name" => $item->attributes()->svc_name, "protocol" => $item->attributes()->protocol, "pluginID" => $item->attributes()->pluginID, "pluginName" => $item->attributes()->pluginName, "pluginFamily" => $item->attributes()->pluginFamily, "vuln_publication_date" => $item->vuln_publication_date, "cpe" => $item->cpe, "solution" => $item->solution, "risk_factor" => $item->risk_factor, "description" => $item->description, "plugin_publication_date" => $item->plugin_publication_date, "cvss_vector" => $item->cvss_vector, "synopsis" => $item->synopsis, "plugin_type" => $item->plugin_type, "patch_publication_date" => $item->patch_publication_date, 
+"plugin_modification_date" => $item->plugin_modification_date, "cvss_base_score" => $item->cvss_base_score, "cve" => $item->cve, "bid" => $item->bid, "xref" => $item->xref, "plugin_output" => $item->plugin_output, "plugin_version" => $item->plugin_version);
                                 $lowvulnerabilities[] = $tmpvuln;
                         }
                         if ($item->attributes()->severity == '0') {
-                                $tmpvuln = array("ip" => $theip, "dns" 
-=> $thedns, "port" => $item->attributes()->port, "svc_name" => 
-$item->attributes()->svc_name, "protocol" => 
-$item->attributes()->protocol, "pluginID" => 
-$item->attributes()->pluginID, "pluginName" => 
-$item->attributes()->pluginName, "pluginFamily" => 
-$item->attributes()->pluginFamily);
+                                $tmpvuln = array("ip" => $theip, "dns" => $thedns, "port" => $item->attributes()->port, "svc_name" => $item->attributes()->svc_name, "protocol" => $item->attributes()->protocol, "pluginID" => $item->attributes()->pluginID, "pluginName" => $item->attributes()->pluginName, "pluginFamily" => $item->attributes()->pluginFamily);
                                 $infovulnerabilities = $tmpvuln;
                         }
                 }
@@ -220,8 +149,7 @@ foreach ($critvulnerabilities as &$value) {
         $c_synopsis = $value['synopsis'];
         $c_plugin_type = $value['plugin_type'];
         $c_patch_publication_date = $value['patch_publication_date'];
-        $c_plugin_modification_date = 
-$value['plugin_modification_date'];
+        $c_plugin_modification_date = $value['plugin_modification_date'];
         $c_cvss_base_score = $value['cvss_base_score'];
         $c_cve = $value['cve'];
         $c_bid = $value['bid'];
